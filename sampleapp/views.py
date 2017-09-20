46561018f0f91c25be06bb669fbaaa5c24e350e9
Naveen <nkommuri@primehealthcare.com>
2017-09-20 12:06:25 +0530

    comment form validation in views.py

--- sampleapp/views.py
+++ sampleapp/views.py
@@ -8,17 +8,17 @@ from django.views.decorators.csrf import csrf_exempt, csrf_protect
 @csrf_exempt
 def upload(request):
     form = UploadForm(request.POST, request.FILES)
-    _str = "<H2>Failure</h2>"
-    if (form.is_valid()):
-        parseFile(request.FILES["file"])
-        wb = openpyxl.load_workbook('example_out.xlsx')
-        sheet = wb.get_sheet_by_name('Sheet1')
-        Record.objects.all().delete()
-        for i in range(1, sheet.max_row):
-            if sheet.cell(row=i, column=3).value is not None:
-                record = Record(name=str(sheet.cell(row=i, column=2).value), amount=sheet.cell(row=i, column=3).value)
-                record.save()
-                _str = _str + "<h2>" + record.name + " - " + str(record.amount) + "</h2><br/>"
+    _str = ""
+    # if form.is_valid():
+    parseFile(request.FILES["file"])
+    wb = openpyxl.load_workbook('example_out.xlsx')
+    sheet = wb.get_sheet_by_name('Sheet1')
+    Record.objects.all().delete()
+    for i in range(1, sheet.max_row):
+        if sheet.cell(row=i, column=3).value is not None:
+            record = Record(name=str(sheet.cell(row=i, column=2).value), amount=sheet.cell(row=i, column=3).value)
+            record.save()
+            _str = _str + "<h2>" + record.name + " - " + str(record.amount) + "</h2><br/>"
     return HttpResponse(_str)
 
 
