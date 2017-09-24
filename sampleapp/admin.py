55ee9fb93e620a82eec4de7dea0e4fc2f65dfcf8
Naveen kommuri <howru42@gmail.com>
2017-09-24 22:54:11 +0530

    Add testUser(test/test@123) and display all columns

--- sampleapp/admin.py
+++ sampleapp/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Record
 
-admin.site.register(Record)
+
+class DoctorsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount')
+
+
+admin.site.register(Record, DoctorsAdmin)
